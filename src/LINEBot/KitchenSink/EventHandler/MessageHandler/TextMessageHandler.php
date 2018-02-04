a501cb6de684058d034f34f80d624c8493f53a60
bryandito <bryanditowicak@gmail.com>
2018-02-04 15:39:00 +0700

    tambamenuq

--- src/LINEBot/KitchenSink/EventHandler/MessageHandler/TextMessageHandler.php
+++ src/LINEBot/KitchenSink/EventHandler/MessageHandler/TextMessageHandler.php
@@ -129,6 +129,10 @@ class TextMessageHandler implements EventHandler
                         new PostbackTemplateActionBuilder('Add to cart', 'action=add&itemid=123'),
                         new MessageTemplateActionBuilder('Say message', 'coba'),
                     ]),
+                    new CarouselColumnTemplateBuilder('tanya', 'siapa', $imageUrl, [
+                        new PostbackTemplateActionBuilder('Add to cart', 'action=add&itemid=123'),
+                        new MessageTemplateActionBuilder('Say message', 'coba'),
+                    ]),
                 ]);
                 $templateMessage = new TemplateMessageBuilder('Button alt text', $carouselTemplateBuilder);
                 $this->bot->replyMessage($replyToken, $templateMessage);
